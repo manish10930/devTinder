eac396ee12418d7cd8214737af834121297a8e51
manish kumar <manishkumar10930@gmail.com>
2025-03-15 12:00:38 +0530

    latest code pushed

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
+// const DB_USER="manishkumar10930"
+// const DB_PASSWORD="Manish%40123"
+
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
